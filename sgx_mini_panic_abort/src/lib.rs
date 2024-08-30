8869ad959302b4874ab61369313811ed59a244ad
Michael Spiegel <michael@hushmesh.com>
2024-08-30 16:23:38 -0400

    Add sgx_mini_panic_abort

---
+++ sgx_mini_panic_abort/src/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![cfg(not(test))]
+
+use core::panic::PanicInfo;
+
+use log::error;
+
+#[link(name = "sgx_trts")]
+extern "C" {
+    pub fn abort() -> !;
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    error!("Panic: {}", info);
+    log::logger().flush();
+    unsafe {
+        abort();
+    }
+}
