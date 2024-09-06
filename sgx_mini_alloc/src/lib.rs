2c6bea3c8055a6e395e7926d6b6e6cede2e4d637
Michael Spiegel <michael@hushmesh.com>
2024-09-06 13:58:35 -0400

    try a different allocator api

--- sgx_mini_alloc/src/lib.rs
+++ sgx_mini_alloc/src/lib.rs
@@ -1,24 +1,21 @@
 #![no_std]
 
 use core::alloc::GlobalAlloc;
-use core::ptr::write_bytes;
 
-use sgx_mini_types::sgx_aligned_free;
-use sgx_mini_types::sgx_aligned_malloc;
+extern "C" {
+    // pub fn malloc(size: usize) -> *mut u8;
+    pub fn memalign(align: usize, size: usize) -> *mut u8;
+    pub fn free(p: *mut u8);
+}
 
 pub struct SgxMiniAlloc;
 
 unsafe impl GlobalAlloc for SgxMiniAlloc {
     unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
-        unsafe {
-            sgx_aligned_malloc(layout.size(), layout.align(), core::ptr::null(), 0) as *mut u8
-        }
+        unsafe { memalign(layout.align(), layout.size()) }
     }
 
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
-        unsafe {
-            write_bytes(ptr, 0u8, layout.size());
-            sgx_aligned_free(ptr as *mut core::ffi::c_void);
-        }
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: core::alloc::Layout) {
+        unsafe { free(ptr) }
     }
 }
