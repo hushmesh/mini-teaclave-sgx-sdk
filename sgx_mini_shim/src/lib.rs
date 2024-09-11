e19f8d4a5d46edb39dcfbc8f7fb4a157c663147b
Michael Spiegel <michael@hushmesh.com>
2024-09-11 09:10:16 -0400

    update shim

--- sgx_mini_shim/src/lib.rs
+++ sgx_mini_shim/src/lib.rs
@@ -79,3 +79,106 @@ pub extern "C" fn u_dirfd_ocall(_error: *mut c_int, _dirp: *mut c_char) -> c_int
 pub extern "C" fn u_closedir_ocall(_error: *mut c_int, _dirp: *mut c_char) -> c_int {
     0
 }
+
+#[no_mangle]
+pub extern "C" fn u_readdir64_r_ocall(
+    _dirp: *mut c_char,
+    _entry: *mut c_char,
+    _result: *mut *mut c_char,
+) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_opendir_ocall(_error: *mut c_int, _pathname: *const c_char) -> *mut c_char {
+    core::ptr::null_mut()
+}
+
+#[no_mangle]
+pub extern "C" fn u_fdopendir_ocall(_error: *mut c_int, _fd: c_int) -> *mut c_char {
+    core::ptr::null_mut()
+}
+
+#[no_mangle]
+pub extern "C" fn u_rmdir_ocall(_error: *mut c_int, _pathname: *const c_char) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_mkdir_ocall(_error: *mut c_int, _pathname: *const c_char, _mode: u16) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_realpath_ocall(_error: *mut c_int, _pathname: *const c_char) -> *mut c_char {
+    core::ptr::null_mut()
+}
+
+#[no_mangle]
+pub extern "C" fn u_symlink_ocall(
+    _error: *mut c_int,
+    _path1: *const c_char,
+    _path2: *const c_char,
+) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_readlink_ocall(
+    _error: *mut c_int,
+    _path: *const c_char,
+    _buf: *mut c_char,
+    _bufsz: usize,
+) -> usize {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_chmod_ocall(_error: *mut c_int, _path: *const c_char, _mode: u16) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_rename_ocall(
+    _error: *mut c_int,
+    _oldpath: *const c_char,
+    _newpath: *const c_char,
+) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_linkat_ocall(
+    _error: *mut c_int,
+    _olddirfd: c_int,
+    _oldpath: *const c_char,
+    _newdirfd: c_int,
+    _newpath: *const c_char,
+    _flags: c_int,
+) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_unlinkat_ocall(
+    _error: *mut c_int,
+    _dirfd: c_int,
+    _pathname: *const c_char,
+    _flags: c_int,
+) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_link_ocall(
+    _error: *mut c_int,
+    _oldpath: *const c_char,
+    _newpath: *const c_char,
+) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub extern "C" fn u_unlink_ocall(_error: *mut c_int, _pathname: *const c_char) -> c_int {
+    0
+}
